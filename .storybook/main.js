90f4ebc3995d4fee808629304f5a1dd889d71af7
Haruki <kohei99624@gmail.com>
2023-02-03 21:02:41 -0500

    add storybook boilerplate

---
+++ .storybook/main.js
@@ -0,0 +1,23 @@
+module.exports = {
+  stories: ['../src/**/*.stories.mdx', '../src/**/*.stories.@(js|jsx|ts|tsx)'],
+  addons: [
+    '@storybook/addon-a11y',
+    '@storybook/addon-actions',
+    '@storybook/addon-backgrounds',
+    '@storybook/addon-docs',
+    '@storybook/addon-essentials',
+    '@storybook/addon-interactions',
+    '@storybook/addon-jest',
+    '@storybook/addon-links',
+    '@storybook/addon-measure',
+    '@storybook/addon-outline',
+    '@storybook/addon-viewport',
+    '@storybook/addon-storysource',
+    '@chakra-ui/storybook-addon',
+    'storybook-dark-mode',
+  ],
+  framework: '@storybook/react',
+  core: {
+    builder: '@storybook/builder-webpack5',
+  },
+}
