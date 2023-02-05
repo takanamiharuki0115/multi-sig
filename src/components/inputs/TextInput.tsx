db707cea1ebe6a1edf260f1d396e467fc764afef
Haruki <kohei99624@gmail.com>
2023-02-05 15:20:23 -0500

    setup lint, husky

--- src/components/inputs/TextInput.tsx
+++ src/components/inputs/TextInput.tsx
@@ -3,11 +3,40 @@ import { Input } from '@chakra-ui/react'
 
 interface TextInputProps {
   placeholder: string
+  defaultValue?: string
+  value?: string
+  onChange?: (event: React.ChangeEvent<HTMLInputElement>) => void
+  isDisabled?: boolean
+  isReadOnly?: boolean
+  isInvalid?: boolean
 }
 
-const TextInput: React.FC<TextInputProps> = ({ placeholder }) => {
+const TextInput: React.FC<TextInputProps> = ({
+  placeholder,
+  defaultValue,
+  value,
+  onChange,
+  isDisabled,
+  isReadOnly,
+  isInvalid,
+}) => {
   return (
-    <Input w='94%' p={4} m={2} mt={4} borderRadius={10} bg='cyan.100' boxShadow='dark-lg' placeholder={placeholder} />
+    <Input
+      w='94%'
+      p={4}
+      m={2}
+      mt={4}
+      borderRadius={10}
+      bg='cyan.100'
+      boxShadow='dark-lg'
+      placeholder={placeholder}
+      defaultValue={defaultValue}
+      value={value}
+      onChange={onChange}
+      isDisabled={isDisabled}
+      isReadOnly={isReadOnly}
+      isInvalid={isInvalid}
+    />
   )
 }
 
