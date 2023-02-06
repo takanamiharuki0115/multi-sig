e6ccdfe6abdef8ea53887c8b44dbd04365610f5b
Haruki <kohei99624@gmail.com>
2023-02-05 21:28:57 -0500

    add models, constants and states for multiSigs

---
+++ src/constants/multiSigFactory.ts
@@ -0,0 +1,14 @@
+import { MultiSigFactory } from '../models/MultiSigs'
+
+const multiSigFactories: MultiSigFactory[] = [
+  {
+    chainId: 31337,
+    chainName: 'localhost',
+    address: '0x8B190573374637f144AC8D37375d97fd84cBD3a0',
+    name: 'MyMultiSigFactory',
+    version: '0.0.1',
+    multiSigCount: 0,
+  },
+]
+
+export default multiSigFactories
