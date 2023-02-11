569f8f5250331477b4e1eda623afb03708c9b4b4
Haruki <kohei99624@gmail.com>
2023-02-11 15:03:39 -0500

    improve theme

--- src/styles/theme.ts
+++ src/styles/theme.ts
@@ -1,26 +1,54 @@
-import { extendTheme, type ThemeConfig } from '@chakra-ui/react'
+import { extendTheme, defineStyleConfig, type ThemeConfig } from '@chakra-ui/react'
+import { mode, StyleFunctionProps } from '@chakra-ui/theme-tools'
 
 const config: ThemeConfig = {
-  initialColorMode: 'system',
-  useSystemColorMode: true,
+  initialColorMode: 'light',
+  useSystemColorMode: true
 }
 
 const theme = extendTheme({
   config,
   styles: {
-    global: {
-      body: {
-        bgGradient: [
-          'linear(to-tr, teal.300, yellow.400)',
-          'linear(to-t, blue.200, teal.500)',
-          'linear(to-b, orange.100, cyan.300)',
-        ],
-        color: '#000000',
+    // eslint-disable-next-line @typescript-eslint/no-explicit-any
+    global: (props: Record<string, any> | StyleFunctionProps) => ({
+      'html, body': {
+        bgGradient: mode(['linear(to-t, gray.400, gray.200)'], ['linear(to-t, gray.900, gray.700)'])(props),
+        color: mode('#000000', '#ffffff')(props),
+        fontSize: 'sm',
+        lineHeight: 'tall',
         fontFamily: 'Roboto, sans-serif',
-        height: '100vh',
+        height: 'fit-content'
       },
-    },
+      a: {
+        color: mode('#000000', '#ffffff')(props),
+        _hover: {
+          textDecoration: 'underline'
+        }
+      }
+    })
   },
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
+  components: {
+    Card: defineStyleConfig({
+      baseStyle: {
+        borderRadius: 10,
+        fontSize: 'sm',
+        boxShadow: 'dark-lg',
+        bgGradient: ['linear(to-r, cyan.200, cyan.500, purple.200)']
+      },
+      sizes: {
+        sm: {
+          fontSize: 'sm'
+        },
+        md: {
+          fontSize: 'md'
+        }
+      },
+      defaultProps: {
+        size: 'md'
+      }
+    })
+  }
 })
 
 export default theme
