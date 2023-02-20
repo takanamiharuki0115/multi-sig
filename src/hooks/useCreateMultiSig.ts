fd266f998487acdd7c928b7596248b366a766068
Haruki <kohei99624@gmail.com>
2023-02-20 12:19:25 -0500

    improve write hooks

--- src/hooks/useCreateMultiSig.ts
+++ src/hooks/useCreateMultiSig.ts
@@ -1,17 +1,11 @@
-import {
-  useNetwork,
-  usePrepareContractWrite,
-  useContractWrite,
-  useTransaction,
-  useWaitForTransaction,
-  useContractEvent
-} from 'wagmi'
+import { useNetwork, usePrepareContractWrite, useContractEvent } from 'wagmi'
 import { BigNumber } from 'ethers'
 import MyMultiSigFactory from 'mymultisig-contract/abi/MyMultiSigFactory.json'
 
 import { MultiSigConstructorArgs } from '../models/MultiSigs'
 import useMultiSigs from '../states/multiSigs'
 import { useNotification } from './notifications'
+import useFinalizeTransaction from './useFinalizeTransaction'
 
 const useCreateMultiSig = (constructorArgs: MultiSigConstructorArgs, multiSigFactoryAddress: `0x${string}`) => {
   const { chain } = useNetwork()
@@ -24,36 +18,8 @@ const useCreateMultiSig = (constructorArgs: MultiSigConstructorArgs, multiSigFac
     functionName: 'createMultiSig',
     args: [constructorArgs.contractName, constructorArgs.owners, constructorArgs.threshold]
   })
-  const { data, error, isError, isIdle, isLoading, isSuccess, write, writeAsync, reset, status } = useContractWrite({
-    ...config,
-    onError(error) {
-      console.error('Error', error)
-      notificationError()
-    }
-  })
-  useTransaction({
-    hash: data?.hash,
-    enabled: !!data?.hash,
-    cacheTime: 1_00,
-    staleTime: 2_000,
-    onSuccess() {
-      notificationInfo()
-    }
-  })
-  const { data: dataFinal, isSuccess: isFinal } = useWaitForTransaction({
-    hash: data?.hash,
-    enabled: !!data?.hash,
-    confirmations: 1,
-    timeout: 120_000,
-    cacheTime: 1_000,
-    staleTime: 2_000,
-    onSuccess() {
-      notificationSuccess()
-    },
-    onError() {
-      notificationError()
-    }
-  })
+  const { data, error, isError, isIdle, isLoading, isSuccess, write, writeAsync, reset, status, dataFinal, isFinal } =
+    useFinalizeTransaction(config, notificationInfo, notificationSuccess, notificationError)
   useContractEvent({
     address: multiSigFactoryAddress,
     abi: MyMultiSigFactory,
